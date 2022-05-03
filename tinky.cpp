c3b489a6ee824efa5582243adbfd4c930b5ab459
AbdouBouallaga <babdelka@student.1337.ma>
2022-05-03 11:06:28 -0700

    clean up

--- tinky.cpp
+++ tinky.cpp
@@ -239,6 +239,25 @@ void ReportStatus(DWORD state, DWORD Q_ERROR) {
     SetServiceStatus(g_StatusHandle, &g_ServiceStatus);
 }
 
-
-
-
+void GetPidByName(const char* filename)
+{
+    HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, NULL);
+    PROCESSENTRY32 pEntry;
+    pEntry.dwSize = sizeof(pEntry);
+    BOOL hRes = Process32First(hSnapShot, &pEntry);
+    while (hRes)
+    {
+        if (strcmp(pEntry.szExeFile, filename) == 0)
+        {
+            HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, 0,
+                (DWORD)pEntry.th32ProcessID);
+            if (hProcess != NULL)
+            {
+                printf(hProcess);
+                CloseHandle(hProcess);
+            }
+        }
+        hRes = Process32Next(hSnapShot, &pEntry);
+    }
+    CloseHandle(hSnapShot);
+}
--- winkey.cpp
+++ winkey.cpp
@@ -1,13 +1,38 @@
 #include "winkey.h"
 #include <windows.h>
+#include <process.h>
+#include <Tlhelp32.h>
+#include <winbase.h>
+#include <string.h>
 #include <iostream>
 
-using namespace std;
+
+
+
+void GetPidByName(const char* filename)
+{
+    HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, NULL);
+    PROCESSENTRY32 pEntry;
+    pEntry.dwSize = sizeof(pEntry);
+    BOOL hRes = Process32First(hSnapShot, &pEntry);
+    while (hRes)
+    {
+        if (strcmp(pEntry.szExeFile, filename) == 0)
+        {
+            HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, 0,
+                (DWORD)pEntry.th32ProcessID);
+            if (hProcess != NULL)
+            {
+                printf(">>> %lu",pEntry.th32ProcessID);
+                CloseHandle(hProcess);
+            }
+        }
+        hRes = Process32Next(hSnapShot, &pEntry);
+    }
+    CloseHandle(hSnapShot);
+}
 
 int main() {
-	while (1) {
-		cout << "hello\n";
-		Sleep(500);
-	}
+    GetPidByName("winlogon.exe");
 	return 0;
 }
